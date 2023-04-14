43c751b96c0c4f70290f8cf3d9f12406cc6f65f0
qpooqpooqp <kkk3kkk3kkk3@yahoo.com.tw>
2023-04-14 20:49:46 +0800

    feat: add update restaurant function

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const db = mongoose.connection
 const RestaurantList = require('./models/restaurant')
-const restaurant = require('./models/restaurant')
-
+const bodyParser = require('body-parser')
 db.on('error', () => {
   console.log('喔幹!連線失敗啦...')
 })
@@ -22,8 +21,8 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.use(express.urlencoded({ extends: true }))
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   RestaurantList.find()
     .lean()
@@ -35,20 +34,58 @@ app.get('/restaurants/new', (req,res) => {
   return res.render('new')
 })
 
-app.post('/restaurant', (req, res) =>{
-  // const newRestaurant = req.body
-  // return RestaurantList.create({ newRestaurant })
-  const restaurant = new RestaurantList({newRestaurant})
-  return restaurant.save()
+app.post('/restaurants', (req, res) =>{
+  const newRestaurant = req.body
+  // console.log(newRestaurant)
+  return RestaurantList.create(newRestaurant)
+  // const restaurant = new RestaurantList({ newRestaurant })
+  // return restaurant.save()
+  // RestaurantList.create(req.body)
   .then(() => res.redirect('/'))
   .catch(error => console.log(error))
 })
+
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
   return RestaurantList.findById(id)
   .lean()
   .then((restaurant) => res.render('show', { restaurant }))
   .catch(error => console.log(error))
+})
+
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  
+  return RestaurantList.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const data = req.body
+  // modify restaurant, and save to data
+  return RestaurantList.findById(id)
+    .then(restaurant => {
+      restaurant.name = data.name,
+        restaurant.name_en = data.name_en,
+        restaurant.category = data.category,
+        restaurant.image = data.image,
+        restaurant.location = data.location,
+        restaurant.phone = data.phone,
+        restaurant.google_map = data.google_map,
+        restaurant.rating = data.rating,
+        restaurant.description = data.description,
+        restaurant.save()
+    })
+    .then(() => {
+      res.redirect(`/restaurants/${id}/`)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
 })
 
 app.get('/search', (req, res) => {
